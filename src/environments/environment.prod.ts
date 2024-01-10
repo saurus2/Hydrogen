4062667829d96ca947ca0f889139fc5e67d47c1f
Jihyeok Choi <jihyeokchoi@Jihyeoks-Mac-mini.local>
2024-01-09 23:08:24 -0800

    Title: Added API & services Content: Register Google map api key, ionic service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  // API Key
+  googleMapsApiKey: 'AIzaSyBVS8gDuHKa4L6W66_DDVR_m5NnNDWzYJ4'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // API Key
+  googleMapsApiKey: 'AIzaSyBVS8gDuHKa4L6W66_DDVR_m5NnNDWzYJ4'
 };
 
 /*
