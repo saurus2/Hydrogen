49e6dc8cc64920c8d8994d29445001560c98d414
Jihyeok Choi <jihyeokchoi@Jihyeoks-Mac-mini.local>
2024-02-07 23:02:00 -0800

    Title: Complete loading google map with API Contents: API Key restriction was necessary and ENABLE for Javascript Google map api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { GmapsService } from '../services/gamps/gmaps.service';
 
 @Component({
@@ -6,19 +6,38 @@ import { GmapsService } from '../services/gamps/gmaps.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
   
-  @ViewChild('map', {static: true}) mapElementRef: ElementRef;
+  @ViewChild('map', { static: true })
+  mapElementRef!: ElementRef;
   googleMaps: any;
+  center = { lat: 28.649944693035188, lng: 77.23961776224988 };
+  map: any;
 
   constructor(
-    private gmaps: GmapsService
+    private gmaps: GmapsService,
+    private renderer: Renderer2,
   ) {}
   
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(){
+    this.loadMap();
+  }
+
   async loadMap() {
     try {
       let googleMaps: any = await this.gmaps.loadGoogleMaps();
-
+      this.googleMaps = googleMaps;
+      const mapEl = this.mapElementRef.nativeElement;
+      const location = new googleMaps.LatLng(this.center.lat, this.center.lng);
+      this.map = new googleMaps.Map(mapEl, {
+        center: location,
+        zoom: 12,
+      });
+      this.renderer.addClass(mapEl, 'visible');
     } catch(e) {
       console.log(e);
     }
