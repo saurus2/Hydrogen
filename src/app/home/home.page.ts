c829e11be136b473d3e225b2a54630b28a3b8940
Jihyeok Choi <jihyeokchoi@Jihyeoks-Mac-mini.local>
2024-03-07 22:57:45 -0800

    add marker

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,6 +13,9 @@ export class HomePage implements OnInit{
   googleMaps: any;
   center = { lat: 28.649944693035188, lng: 77.23961776224988 };
   map: any;
+  mapClickListener: any;
+  markerClickListener: any;
+  markers: any[] = [];
 
   constructor(
     private gmaps: GmapsService,
@@ -38,9 +41,51 @@ export class HomePage implements OnInit{
         zoom: 12,
       });
       this.renderer.addClass(mapEl, 'visible');
+      this.addMarker(location);
+      this.onMapClick();
     } catch(e) {
       console.log(e);
-    }
-    
+    } 
   }
+
+  onMapClick(){
+    this.mapClickListener = this.googleMaps.event.addListener(this.map, "click", (mapMouseEvent: { latLng: { toJSON: () => any; }; }) => {
+      console.log("hi",mapMouseEvent.latLng.toJSON());
+      this.addMarker(mapMouseEvent.latLng);
+    });
+  }
+
+  addMarker(location: any){
+    let googleMaps: any = this.googleMaps;
+    const icon = {
+      url: 'assets/icon/pin.png',
+      scaledSize: new googleMaps.Size(35, 50),
+    };
+    console.log("hello", location);
+    const marker = new googleMaps.Marker({
+      position: location,
+      map: this.map,
+      icon: icon,
+      draggable: true,
+    });
+    this.markers.push(marker);
+    this.markerClickListener = this.googleMaps.event.addListener(marker, "click", () => {
+      console.log('markerclick', marker);
+      this.checkAndRemoveMarker(marker);
+      console.log('markers: ', this.markers);
+    });
+  }
+
+  checkAndRemoveMarker(marker: any) {
+    const index = this.markers.findIndex(x => x.position.lat() == this.markerClickListener.position.lat && x.position.lng == marker.position.lng);
+    console.log('is marker already: ', index);
+    if(index >= 0) {
+      this.markers[index].setMap(null);
+      this.markers.splice(index, 1);
+      return;
+    }
+  }
+
+  
+
 }
